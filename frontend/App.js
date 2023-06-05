a6b7e9e09d27195981e65f28215fa92bbcf18356
Aksel-R <yassinrezgui19gmail.com>
2023-06-05 16:07:08 +0100

    fixed the home page bug

--- frontend/App.js
+++ frontend/App.js
@@ -4,13 +4,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Profile from "./screens/profile1"
 
-import React from 'react';
 import Login from './screens/login/login';
 import SignUp from './screens/signup/signUp';
 import SignUp2 from './screens/signup/signup2';
-
-import  Acceuil from './screens/Acceuil'
+import Tabs from './navigation/TabNav';
+import  Home from './screens/Acceuil'
 import TabNav from './navigation/TabNav'
 
 
@@ -27,29 +27,14 @@ export default function App() {
         <Stack.Screen name="Signup" component={SignUp} options={{ title: 'SignUp',headerShown: false  }} />
         <Stack.Screen name="login" component={Login} options={{ title: 'Login',headerShown: false }} />
         <Stack.Screen name="SignUp2" component={SignUp2} options={{ title: 'SignUp2',headerShown: false }} />
-  <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Welcome' }} />
-        <Stack.Screen name="Acceuil" component={Acceuil} options={{ title: 'Acceuil',headerShown: false  }} />
+        {/* <Stack.Screen name="ProfileComponent" component={Profile} options={{ title: 'ProfileComponent',headerShown: false }} /> */}
+  
+        <Stack.Screen name="Acceuil" component={Home} options={{ title: 'Acceuil',headerShown: false  }} />
         <Stack.Screen name="TabNav" component={TabNav} options={{ headerShown: false  }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const HomeScreen = () => {
-  const navigation = useNavigation();
-
-  return (
-    <View style={styles.container}>
-      <Text color="black">Hello, world!</Text>
-      <StatusBar style="auto" />
-      <Image source={require('./assets/qq.png')} />
-      <Button title="get started" onPress={() => navigation.navigate('Signup')} />
-    </View>
-  );
-};
-
-
-
 const HomeScreen = () => {
   const navigation = useNavigation();
 
@@ -58,13 +43,10 @@ const HomeScreen = () => {
       {/* <Text color="black">Hello, world!</Text> */}
       <StatusBar styles="auto" />
       <Image source={require('./assets/qq.png')} />
-      <Button  title="Acceuil " onPress={() => navigation.navigate('TabNav',{name:'Acceuil'})} style={styles.button} />
+      <Button  title="Acceuil " onPress={() => navigation.navigate('TabNav')} style={styles.button} />
     </View>
   );
 };
-
-
-
 
 const styles = StyleSheet.create({
   container: {
