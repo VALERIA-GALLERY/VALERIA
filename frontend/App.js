d4a965f91c7891bc3bab09477ca3fc36a45af487
Mohamedaziz1919 <guerfalamohamed2001@gmail.com>
2023-06-06 02:58:40 +0200

    *

--- frontend/App.js
+++ frontend/App.js
@@ -61,16 +61,4 @@ const styles = StyleSheet.create({
   button:{
     color:'#5B3D00'
   }
-
-});
-
-import { View, Text } from 'react-native'
-import React from 'react'
-
-export default function App() {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+})
